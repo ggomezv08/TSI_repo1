cf6f18428db8bab20275ed20d42b500d22a27704
TeHMizo <72949835+TeHMizo@users.noreply.github.com>
2020-11-24 18:29:17 -0500

    Cambio nombre
    Cambie el nombre de mi rama de mi_lib a mi_libMaizo y de principal a principalMaizo

--- principalMaizo.py
+++ principalMaizo.py
@@ -1,7 +1,7 @@
-import mi_lib
+import mi_libMaizo
 
 a = int(input("Ingrese un número: "))
-if mi_lib.capicua (a) == True:
+if mi_libMaizo.capicua (a) == True:
     print("Es un número capicua")
 else:
     print("No es un número capicua")
