4062811d82c733ea036dbd6e4ae83a2f39c3391f
TeHMizo <72949835+TeHMizo@users.noreply.github.com>
2020-11-24 18:16:08 -0500

    Función capicua añadida
    Se desarrollo la función capicua en mi_lib y se comprobo en principal

---
+++ mi_lib.py
@@ -0,0 +1,5 @@
+def capicua (num):
+    x=str(num)
+    lista=list(x)
+    if lista==lista[::-1]:
+        return True
---
+++ principal.py
@@ -0,0 +1,12 @@
+import mi_lib
+
+a = int(input("Ingrese un número: "))
+if mi_lib.capicua (a) == True:
+    print("Es un número capicua")
+else:
+    print("No es un número capicua")
+
+
+
+
+
